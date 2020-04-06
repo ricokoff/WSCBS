7c971012d058b343a094a54accf3a2de93fad959
ricokoff <ricokoff@hotmail.com>
2020-04-06 18:52:27 +0200

    Working REST URL shortener

--- RESTURLshortener/src/net/codejava/URLshortener.java
+++ RESTURLshortener/src/net/codejava/URLshortener.java
@@ -4,38 +4,36 @@ package net.codejava;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
-
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
-import java.util.*;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 @Path("/shortener")
 public class URLshortener {
 	
-	static int currentID = 1;
-	static Map<String, Integer> idMap = new HashMap<String, Integer>();
-	
+
+	static Map<String, String> idURLMap = new HashMap<String, String>();
+	static Map<String, String> URLidMap = new HashMap<String, String>();
+	private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+	public static String randomAlphaNumeric(int count) {
+		StringBuilder builder = new StringBuilder();
+	while (count-- != 0) {
+	int character = (int)(Math.random()*ALPHA_NUMERIC_STRING.length());
+	builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+	}
+	return builder.toString();
+	}
+
 	public boolean validURL(String url) {
-//		  try {
-//			     (new java.net.URL(url)).openStream().close();
-//			     return true;
-//			  } catch (Exception ex) {
-//			  return false;}
-//		}
        String regex = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
        return IsMatch(url,regex);
     }
@@ -49,23 +47,41 @@ public class URLshortener {
        }  
    }
    
-	public int createID(String longUrl) {
-		if(idMap.containsKey(longUrl)) {
-			return idMap.get(longUrl);			
+   private static String createUniqueCode(int count) {
+	   String code = randomAlphaNumeric(6);
+	   if(!idURLMap.containsKey("lin.k" + code)){
+		   return code;
+	   } else {
+		   return createUniqueCode(6);
+	   }
+   }
+   
+	public String createID(String longUrl) {
+		if(URLidMap.containsKey(longUrl)) {
+			return URLidMap.get(longUrl);			
 		}else {
-		idMap.put(longUrl,currentID);
-		currentID = currentID + 1;
-		return idMap.get(longUrl);
+			String code = createUniqueCode(6);
+		idURLMap.put(("lin.k" + code),longUrl);
+		URLidMap.put(longUrl,("lin.k" + code));
+		return URLidMap.get(longUrl);
 		}
 	}
 	
     @GET
-    @Consumes(MediaType.TEXT_PLAIN)
+    @Path("/urls/{url}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String getrequest(String URL) {
-    	
-    	
-    	return "TEST";
+    public String getrequest(@PathParam("url") String URL) {
+    	if(idURLMap.containsKey(URL)) {
+    		return "HTTP: " + Response.Status.MOVED_PERMANENTLY.getStatusCode() + " " + Response.Status.MOVED_PERMANENTLY + "\n" + idURLMap.get(URL);}
+    	else {
+    		return "HTTP: " + Response.Status.NOT_FOUND.getStatusCode() + " " + Response.Status.NOT_FOUND + "\nshortURL: \"" + URL + "\" cannot be found in our database.";}   
+    }
+    
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String emptygetrequest() {
+    	return "HTTP: " + Response.Status.OK.getStatusCode() + " " + Response.Status.OK + "\n" +
+    				idURLMap.keySet();
     }
     
     @POST
@@ -73,23 +89,52 @@ public class URLshortener {
     @Produces(MediaType.APPLICATION_JSON)
     public String postrequest(String URL) {
     	if(!validURL(URL)) {
-    		return "HTTP: " + Response.Status.BAD_REQUEST.getStatusCode() + " " + Response.Status.BAD_REQUEST + "\n ERROR";}
+    		return "HTTP: " + Response.Status.BAD_REQUEST.getStatusCode() + " " + Response.Status.BAD_REQUEST + "\nThis is not a valid URL. A valid URL has the following format: http://www.{domain name}.{extension}";}
     	else {
-    		int id = createID(URL);
+    		String id = createID(URL);
     		return "HTTP: " + Response.Status.CREATED.getStatusCode() + " " + Response.Status.CREATED + "\nYour ID is: " + id;}
     	}	 
         
     @PUT
+    @Path("/urls/{id}")
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
-    public String putrequest(String URL) {
-        return "This is a PUT request.";
+    public String putrequest(@PathParam("id") String id, String newURL) {
+    	if(!idURLMap.containsKey(id)) {
+    		return "HTTP: " + Response.Status.NOT_FOUND.getStatusCode() + " " + Response.Status.NOT_FOUND + "\nThe shortURL \""+id+"\" cannot be found in our database. Please try again with a valid URL.";
+    	} else if (id.length() != 11) {
+    		return "HTTP: " + Response.Status.BAD_REQUEST.getStatusCode() + " " + Response.Status.BAD_REQUEST + "\nThe shortURL should have the following format: \"lin.k{******}\" where * only consists of alphanumeric values.";
+    	} else if (!validURL(newURL)){
+    		return "HTTP: " + Response.Status.BAD_REQUEST.getStatusCode() + " " + Response.Status.BAD_REQUEST + "\nThis is not a valid URL. A valid URL has the following format: http://www.{domain name}.{extension}";}
+    	  else {
+    		String oldURL = idURLMap.replace(id, newURL);
+    		return "HTTP: "+Response.Status.OK.getStatusCode()+" "+Response.Status.OK+"\nShortURL \""+id+" \" is set from \""+oldURL+"\" to \""+newURL+"\"";
+    	}
+	
     }
     
     @DELETE
+    @Path("/urls/{id}")
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
-    public String deleterequest(String URL) {
-        return "This is a DELETE request.";
+    public String deleterequest(@PathParam("id") String id) {
+    	if(!idURLMap.containsKey(id)) {
+    		return "HTTP: " + Response.Status.NOT_FOUND.getStatusCode() + " " + Response.Status.NOT_FOUND + "\nThe shortURL \""+id+"\" cannot be found in our database. Please try again with a valid URL.";
+    	} else {
+    		String longURL = idURLMap.get(id);
+    		idURLMap.remove(id);
+    		URLidMap.remove(longURL);
+            return"HTTP: "+Response.Status.NO_CONTENT.getStatusCode()+" "+Response.Status.NO_CONTENT +"\nThe shortURL \""+id+"\" is succesfully removed from the database";
+            
+    	}
+
+    }
+    
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    public String emptydeleterequest() {
+    	idURLMap.clear();
+    	URLidMap.clear();
+        return"HTTP: "+Response.Status.NO_CONTENT.getStatusCode()+" "+Response.Status.NO_CONTENT +"\nAll URL mappings are succesfully removed!";
     }
 }
--- URLshortenerClient/src/net/codejava/client.java
+++ URLshortenerClient/src/net/codejava/client.java
@@ -1,5 +1,10 @@
 package net.codejava;
  
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.Scanner;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -9,33 +14,110 @@ import javax.ws.rs.core.MediaType;
 import org.glassfish.jersey.client.ClientConfig;
 public class client {
 
-    public static void main(String[] args) {
-        String uri = "http://localhost:8080/RESTURLshortener/rest/shortener";
-        ClientConfig config = new ClientConfig();
-        Client client = ClientBuilder.newClient(config);
-        WebTarget target = client.target(uri);
-         
-        String firstresponse = target.request(MediaType.TEXT_PLAIN)
-                    .accept(MediaType.TEXT_PLAIN)
-                    .get(String.class);
-        
-        String secondresponse = target.request(MediaType.TEXT_PLAIN)
-                .accept(MediaType.APPLICATION_JSON)
-                .post(Entity.entity("http://www.google.com", MediaType.TEXT_PLAIN), String.class);
-     
-        String thirdresponse = target.request(MediaType.TEXT_PLAIN)
+    static String uri = "http://localhost:8080/RESTURLshortener/rest/shortener";
+    static ClientConfig config = new ClientConfig();
+    static Client client = ClientBuilder.newClient(config);
+	static boolean isKilled = false;
+	
+	public static String sendGetRequest(String request) {
+		WebTarget target = client.target(uri).path("urls").path(request);
+        return target.request(MediaType.TEXT_PLAIN)
                 .accept(MediaType.TEXT_PLAIN)
-                .put(Entity.entity("test", MediaType.TEXT_PLAIN), String.class);
-    
-        String fourthresponse = target.request(MediaType.TEXT_PLAIN)
+                .get(String.class);
+	}
+	
+	public static String sendEmptyGetRequest() {
+		WebTarget target = client.target(uri);
+        return target.request(MediaType.TEXT_PLAIN)
+                .accept(MediaType.TEXT_PLAIN)
+                .get(String.class);
+	}
+	
+	public static String sendPostRequest(String request) {
+		WebTarget target = client.target(uri);
+		return target.request(MediaType.TEXT_PLAIN)
+         .accept(MediaType.APPLICATION_JSON)
+         .post(Entity.entity(request, MediaType.TEXT_PLAIN), String.class);
+	}
+	
+	public static String sendPutRequest(String id, String newURL) {
+		WebTarget target = client.target(uri).path("urls").path(id);
+        return target.request(MediaType.TEXT_PLAIN)
+                .accept(MediaType.TEXT_PLAIN)
+                .put(Entity.entity(newURL, MediaType.TEXT_PLAIN), String.class);
+	}
+	
+	public static String sendDeleteRequest(String id) {
+		WebTarget target = client.target(uri).path("urls").path(id);;
+        return target.request(MediaType.TEXT_PLAIN)
                 .accept(MediaType.TEXT_PLAIN)
                 .delete(String.class);
-    
-        
-        System.out.println("First response is " + firstresponse);
-        System.out.println("Second response is " + secondresponse);
-        System.out.println("Third response is " + thirdresponse);
-        System.out.println("Fourth response is " + fourthresponse);
-    }
+	}
 	
+	public static String sendEmptyDeleteRequest() {
+		WebTarget target = client.target(uri);
+        return target.request(MediaType.TEXT_PLAIN)
+                .accept(MediaType.TEXT_PLAIN)
+                .delete(String.class);
+	}
+	
+	
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		PrintStream out = new PrintStream(System.out);
+		out.println("************************************************************************\n"
+				+ "Welcome to the URLshortener client!\n"
+				+ "Usage: [Command] {Identifier} {URL}\n"
+				+ "Available commands: [GET, POST, PUT, DELETE]\n"
+				+ "Command usage is as follows:\n"
+				+ "To get a new shortURL ID: POST {URL}\n"
+				+ "To retrieve the URL of a shortURL ID: GET {IDENTIFIER}\n"
+				+ "To retrieve all the shortURL ID's currently in the database: GET {}\n"
+				+ "To replace the URL at a specific shortURL ID: PUT {IDENTIFIER} {URL}\n"
+				+ "To delete a shortURL ID: DELETE {IDENTIFIER\n"
+				+ "To delete all the shortURL's: DELETE {}\n"
+				+ "To exit this program: EXIT\n"
+				+ "************************************************************************");
+    	while(!isKilled) {
+    	     String input = "";
+			try {
+				out.println("\nType your command here: ");
+				input = reader.readLine();
+			} catch (IOException e) {
+				out.println("No input received!");
+				e.printStackTrace();
+			}
+    	     if(input.toUpperCase().equals("EXIT")){isKilled = true;}
+    	     else {
+    	    	 Scanner scanner = new Scanner(input);
+    	    	 String command = scanner.next();
+    	    	 if(command.toUpperCase().equals("GET")) {
+    	    		 if(scanner.hasNext()) {
+    	    			 String id = scanner.next();
+    	    			 out.println(sendGetRequest(id));
+    	    		 } else {out.println(sendEmptyGetRequest());}
+    	    	 } else if(command.toUpperCase().equals("POST")){
+    	    		 if(scanner.hasNext()) {
+    	    			 String url = scanner.next();
+    	    			 out.println(sendPostRequest(url));
+    	    		 } else {out.println("Invalid input. Usage: POST {URL}");}
+    	    	 } else if(command.toUpperCase().equals("PUT")){
+    	    		 if(scanner.hasNext()) {
+    	    			 String id = scanner.next();
+    	    			 if(scanner.hasNext()) {
+    	    				 String url = scanner.next();
+    	    				 out.println(sendPutRequest(id, url));
+    	    			 } else {out.println("Invalid input. Usage: PUT {Identifier} {new URL}");}
+    	    		 } else {out.println("Invalid input. Usage: PUT {Identifier} {new URL}");} 
+    	    	 } else if(command.toUpperCase().equals("DELETE")){
+    	    		 if(scanner.hasNext()) {
+    	    			 String id = scanner.next();	
+    	    			 out.println(sendDeleteRequest(id));
+    	    		 } 	else{out.println(sendEmptyDeleteRequest());}
+    	    	 }else {out.println("Usage: [Command] {Identifier} {URL}\nAvailable commands: [GET, POST, PUT, DELETE]");}
+    	     scanner.close();
+    	     }
+    	}	
+    }
 }
+
