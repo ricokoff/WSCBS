f4ff0bf141a47e653c43c6cec07a8e86e6109788
ricokoff <ricokoff@hotmail.com>
2020-04-07 19:59:12 +0200

    ...

--- 18_web_service_1.2/URLshortenerClient/src/net/codejava/client.java
+++ 18_web_service_1.2/URLshortenerClient/src/net/codejava/client.java
@@ -14,11 +14,11 @@ import javax.ws.rs.core.MediaType;
 import org.glassfish.jersey.client.ClientConfig;
 public class client {
 
-    static String uri = "http://localhost:8080/RESTURLshortener/rest/shortener";
+    static String uri = "http://localhost:8080/RESTcalculator/rest/shortener";
+    static String uri2 = "http://localhost:8080/RESTURLshortener/rest/shortener";
     static ClientConfig config = new ClientConfig();
     static Client client = ClientBuilder.newClient(config);
 	static boolean isKilled = false;
-	
 	public static String sendGetRequest(String request) {
 		WebTarget target = client.target(uri).path("urls").path(request);
         return target.request(MediaType.TEXT_PLAIN)
